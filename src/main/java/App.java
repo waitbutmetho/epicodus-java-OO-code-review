ce5a84eb4d9964e66d32016f791e25f26ede4285
Josh <silversevenfox@yahoo.com>
2016-02-19 11:18:55 -0800

    add path and vtl to pass first integration test rootTest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,13 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-  
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
