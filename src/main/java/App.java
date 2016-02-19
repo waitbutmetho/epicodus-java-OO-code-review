8cf7da6749abacfa52f5eecc75246ba61abe23ed
Josh <silversevenfox@yahoo.com>
2016-02-19 11:38:42 -0800

    add paths and vtl files to pass second integration test wordIsCreatedTest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,5 +14,20 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/words/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Word newWord = new Word(name);
+      model.put("word", newWord);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
   }
 }
