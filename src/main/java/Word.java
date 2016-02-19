3828decd0e22a54b4193b68199fe6d93e2b0f06d
Josh <silversevenfox@yahoo.com>
2016-02-19 10:52:51 -0800

    add functionality to pass unit test #6 find()

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -34,5 +34,11 @@ public class Word {
 		instances.clear();
 	}
 
-
+  public static Word find(int id) {
+    try {
+      return instances.get( id - 1 );
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
 }
